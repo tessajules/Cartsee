2d907fa2d75da346e87fb6228dce4e4fa4bdee87
Angela Castanieto <angela@Jeffs-Mac.local>
2015-05-14 23:43:55 -0700

    Displaying order details using ajax

--- static/freshlook.js
+++ static/freshlook.js
@@ -2,11 +2,22 @@ function showVisualization() {
 
     $.get('/visualization', function(user_orders_json) {
       $("#display-div").append(
-        user_orders_json["user_gmail"]);
+        "<h3>" + user_orders_json["user_gmail"] + "</h3>");
+        $("#display-div").append("<ol>")
         for (var i = 0; i < user_orders_json["orders"].length; i++)  {
           $("#display-div").append(
-          user_orders_json["orders"][i]["amazon_fresh_order_id"]);
+          "<li>" + "Order # " +
+          user_orders_json["orders"][i]["amazon_fresh_order_id"] +
+            "<ul>" +
+            "<li>" + "delivery date: " + user_orders_json["orders"][i]["delivery_date"] + "</li>" +
+            "<li>" + "delivery time: " + user_orders_json["orders"][i]["delivery_time"] + "</li>" 
+
+            + "</ul>"
+          + "</li>"
+          );
           }
+        $("#display-div").append("<ol>")
+
 
       }
 
