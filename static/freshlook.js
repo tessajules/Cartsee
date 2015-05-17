f1139b88a381664d1ec652f619900af2ad12e2b8
Angela Castanieto <angela@Jeffs-Mac.(none)>
2015-05-16 19:03:42 -0700

    Have basic D3 example chart working using ajax

--- static/freshlook.js
+++ static/freshlook.js
@@ -43,6 +43,7 @@ function listOrders() {
 
 listOrders();
 
+
 function ordersOverTime() {
     $("#display-div").empty();
 
@@ -69,3 +70,82 @@ function ordersOverTime() {
 }
 
 $("#orders-time").on('click', ordersOverTime);
+
+// D3 AREA CHART BELOW
+//http://stackoverflow.com/questions/19901738/d3-area-chart-using-json-object
+//http://bl.ocks.org/mohamed-ali/ed4772df6dca7a48f678
+
+function showAreaChart(data) {
+
+  console.log(data);
+  var margin = {top: 20, right: 20, bottom: 30, left: 50},
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+
+  var parseDate = d3.time.format("%d-%b-%y").parse;
+
+  var x = d3.time.scale()
+      .range([0, width]);
+
+  var y = d3.scale.linear()
+      .range([height, 0]);
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
+
+  var area = d3.svg.area()
+      .x(function(d) { return x(d.date); })
+      .y0(height)
+      .y1(function(d) { return y(d.close); });
+
+  var svg = d3.select("#display-div").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    data.forEach(function(d) {
+      d.date = parseDate(d.date);
+      d.close = +d.close;
+    });
+
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    y.domain([0, d3.max(data, function(d) { return d.close; })]);
+
+    svg.append("path")
+        .datum(data)
+        .attr("class", "area")
+        .attr("d", area);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Price ($)");
+}
+
+
+
+function getJsonObject() {
+  $("#display-div").empty();
+  $.get('/test1', function(json) {
+    data = json["data"]
+    showAreaChart(data);
+  });
+}
+
+$("#area-chart-button").on('click', getJsonObject);
